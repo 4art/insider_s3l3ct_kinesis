073ca8e3e7b6bacee2532ca0f4416c9d3a9bf7c5
metraf <firsov.tyoma@gmail.com>
2019-06-20 21:47:14 +0200

    fixed converter

--- service/converter.js
+++ service/converter.js
@@ -1,4 +1,7 @@
-exports.convertArrToS3Json = (arr, str = "") => arr.forEach(v => str += JSON.stringify(v))
+exports.convertArrToS3Json = (arr, str = "") => {
+    arr.forEach(v => str += JSON.stringify(v))
+    return str
+}
 exports.convertKeysForS3Json = (promise) => promise.then(arr => arr.map(o => {
     Object.keys(o).forEach(old_key => {
         var new_key = old_key.replace(/\s/g, '_')
