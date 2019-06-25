85c95e075a0f377a4b1a586d05b4610d3b407188
metraf <firsov.tyoma@gmail.com>
2019-06-25 18:22:46 +0200

    fixed cors

--- server/service/helper.js
+++ server/service/helper.js
@@ -2,12 +2,20 @@ exports.getLambdaResponse = bodyString => {
     try {
         return {
             statusCode: 200,
-            body: bodyString
+            body: bodyString,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
     };
     } catch (e) {
         return {
             statusCode: 500,
-            body: e
+            body: e,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
         };
     }
 };
