1a82186f22267309b0d7a0ed6aeeb98b25cf606b
metraf <firsov.tyoma@gmail.com>
2019-06-29 01:32:58 +0200

    small changes in desing

--- insider-gui/src/components/Trades.js
+++ insider-gui/src/components/Trades.js
@@ -6,9 +6,9 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography';
 import makeAnimated from 'react-select/animated';
 import Select from 'react-select';
+import DeleteIcon from '@material-ui/icons/Delete';
 import {getCompaniesDE, getTradesDE} from "../apiService";
 
 const styles = theme => ({
@@ -21,16 +21,32 @@ const styles = theme => ({
         overflowX: 'auto',
     },
     paper: {
-        marginTop: theme.spacing(3),
+        margin: 'auto',
         width: '100%',
         overflowX: 'auto',
         marginBottom: theme.spacing(2),
     },
     table: {
-        width: 10
+        margin: 'auto',
     },
     typography: {
-        margin: "10px"
+        margin: "10px",
+        textAlign: 'center'
+    },
+    selectSearchDiv: {
+        margin: 'auto',
+        textAlign: 'center',
+        paddingTop: 20
+    },
+    selectSearch: {
+        width: 300,
+        textAlign: 'center',
+        margin: 'auto',
+        display: 'inline-block',
+        paddingRight: 10
+    },
+    removeIcon: {
+        cursor: 'pointer'
     }
 });
 
@@ -71,23 +87,33 @@ class Trades extends Component {
         this.setState({...this.state, currentCompany: v}, this.updateTrades)
     }
 
-    isCompanyChosed(){
+    isCompanyChosed() {
         return this.state.currentCompany.value != null
+    }
+
+    removeCurrentCompany() {
+        this.setState({
+            ...this.state, currentCompany: {
+                value: null,
+                label: null
+            }
+        }, this.updateTrades)
     }
 
     getTradesTable() {
         const {classes} = this.props;
         return <div className={classes.root}>
 
-            <div className={classes.typography}>
-                <Typography align='left' variant="h6">Last Trades</Typography>
-            </div>
+            {/*<div className={classes.typography}>*/}
+            {/*<Typography variant="h6">Last Trades</Typography>*/}
+            {/*</div>*/}
             <Table size="small" padding="checkbox" className={classes.table}>
                 <TableHead>
                     <TableRow>
                         {!this.isCompanyChosed() ? <TableCell>ISIN</TableCell> : null}
                         {!this.isCompanyChosed() ? <TableCell align="right">Company</TableCell> : null}
-                        {!this.isCompanyChosed() ? <TableCell align="right">Issuer</TableCell> : <TableCell>Issuer</TableCell>}
+                        {!this.isCompanyChosed() ? <TableCell align="right">Issuer</TableCell> :
+                            <TableCell>Issuer</TableCell>}
                         <TableCell align="right">Position</TableCell>
                         <TableCell align="right">Instrument</TableCell>
                         <TableCell align="right">Typ</TableCell>
@@ -99,9 +125,10 @@ class Trades extends Component {
                 <TableBody>
                     {this.state.trades.map((row, i) => (
                         <TableRow key={`trades_${i}`}>
-                            {!this.isCompanyChosed() ? <TableCell align="right">{row.ISIN}</TableCell> : null}
+                            {!this.isCompanyChosed() ? <TableCell>{row.ISIN}</TableCell> : null}
                             {!this.isCompanyChosed() ? <TableCell align="right">{row.Issuer}</TableCell> : null}
-                            <TableCell align="right">{row["Parties_subject_to_the_notification_requirement"]}</TableCell>
+                            <TableCell
+                                align={!this.isCompanyChosed() ? "right" : "left"}>{row["Parties_subject_to_the_notification_requirement"]}</TableCell>
                             <TableCell align="right">{row["Position_/_status"]}</TableCell>
                             <TableCell align="right">{row["Typ_of_instrument"]}</TableCell>
                             <TableCell align="right">{row["Nature_of_transaction"]}</TableCell>
@@ -118,23 +145,27 @@ class Trades extends Component {
     render() {
         const {classes} = this.props;
         return <Paper className={classes.paper}>
-            <Select
-                closeMenuOnSelect={true}
-                value={this.state.currentCompany}
-                components={makeAnimated}
-                onChange={this.currentCompanyOnChange.bind(this)}
-                options={this.state.companies.map(v => ({value: v.ISIN, label: v.Issuer}))}
-                theme={(theme) => ({
-                    ...theme,
-                    borderRadius: 0,
-                    colors: {
-                        ...theme.colors,
-                        primary25: 'gray',
-                        primary: 'black',
-                    },
-                })
-                }
-            />
+            <div className={classes.selectSearchDiv}>
+                <Select
+                    className={classes.selectSearch}
+                    closeMenuOnSelect={true}
+                    value={this.state.currentCompany}
+                    components={makeAnimated}
+                    onChange={this.currentCompanyOnChange.bind(this)}
+                    options={this.state.companies.map(v => ({value: v.ISIN, label: v.Issuer}))}
+                    theme={(theme) => ({
+                        ...theme,
+                        borderRadius: 0,
+                        colors: {
+                            ...theme.colors,
+                            primary25: 'gray',
+                            primary: 'black',
+                        },
+                    })
+                    }
+                />
+                <DeleteIcon onClick={this.removeCurrentCompany.bind(this)}/>
+            </div>
             {this.getTradesTable()}
         </Paper>
     }
