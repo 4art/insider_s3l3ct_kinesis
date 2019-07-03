54bdcfcb4945e61f8dcdae9229184b67c285ee13
metraf <firsov.tyoma@gmail.com>
2019-07-04 00:39:57 +0200

    fixed tests

--- server/app_test.js
+++ server/app_test.js
@@ -2,23 +2,24 @@ var assert = require('assert');
 const converter = require('./service/converter')
 
 
-describe( 'converter', function() {
- 
-    it( 'bafinMoneyToObject', function() {
+describe('converter', function () {
+
+    it('bafinMoneyToObject', function () {
         const strVal = "6.218.640,02 CAD";
         const floatVal = 6218640.02;
         const currency = "CAD"
         let object = converter.bafinMoneyToObject(strVal);
-        assert(object.value, floatVal)
-        assert(object.currency, currency)
+        assert(object.value === floatVal)
+        assert(object.currency === currency)
     });
 
-    it( 'bafinStringDate', function() {
+    it('bafinStringDate', function () {
         const strDate = "26/06/2019";
-        const strDateTime = "26/06/2019 18:06:09";
+        const strDateTime = "03/07/2019 15:39:36";
         let date = converter.bafinStringDate(strDate);
-        assert(date.getDate(), new Date(2019, 5, 26).getDate())
+        assert(date.getTime() === new Date(2019, 5, 26).getTime())
         let dateTime = converter.bafinStringDate(strDateTime);
-        assert(dateTime.getDate(), new Date(2019, 5, 26, 18, 6, 9).getDate())
+        console.log(dateTime)
+        assert(dateTime.getTime() === new Date(2019, 6, 3, 15, 39, 36).getTime(), "same time")
     });
 });
--- server/service/converter.js
+++ server/service/converter.js
@@ -31,6 +31,8 @@ exports.isScvValidUrl = URL => new Promise((res, rej) => {
 exports.getDiffFromTwoArr = (oldArr = [], newArr = []) => oldArr.length === newArr.length ? [] : newArr.filter(v => underscore.isUndefined(oldArr.find(o => underscore.isEqual(v, o))));
 
 exports.bafinMoneyToObject = moneyString => {
+    if(typeof moneyString !== "string") return moneyString
+    if (!moneyString) return {value: 0, currency: ""}
     const preparedString = moneyString.replace(/\./g, "").replace(",", ".");
     return {
         value: parseFloat(preparedString),
@@ -39,16 +41,26 @@ exports.bafinMoneyToObject = moneyString => {
 };
 
 exports.bafinStringDate = stringDate => {
-    if(stringDate.match(/(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{4} (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]/)){
+    if (stringDate.match(/(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{4} (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]/)) {
         const [day, month, yearTime] = stringDate.split("/");
         let split = yearTime.split(" ");
         const year = split[0];
         const [hours, minutes, seconds] = split[1].split(":");
         return new Date(year, month - 1, day, hours, minutes, seconds)
     }
-    if(stringDate.match(/(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{4}/)) {
+    if (stringDate.match(/(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{4}/)) {
         const [day, month, year] = stringDate.split("/");
         return new Date(year, month - 1, day)
     }
     return new Error("Can't parse string date")
 };
+
+exports.convertDataFormatForTrades = trades => trades.map(v => ({
+    ...v,
+    "Averrage_price": exports.bafinMoneyToObject(v["Averrage_price"]).value,
+    currency: exports.bafinMoneyToObject(v["Averrage_price"]).currency,
+    "Aggregated_volume": exports.bafinMoneyToObject(v["Aggregated_volume"]).value,
+    "Date_of_notification": exports.bafinStringDate(v["Date_of_notification"]),
+    "Date_of_transaction": exports.bafinStringDate(v["Date_of_transaction"]),
+    "Date_of_activation": exports.bafinStringDate(v["Date_of_activation"])
+}))
